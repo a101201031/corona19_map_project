825f46ba7c70b12e087e2ea8d4779e9754d4e225
a101201031 <0825tngus@naver.com>
2020-05-26 23:44:42 +0900

    fix : save info as json text file

--- src/coronaInfo/getSeoulCoronaInfo.js
+++ src/coronaInfo/getSeoulCoronaInfo.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 // set data directory
-const dataDir = path.join(__dirname, 'data');
+const dataDir = path.join(__dirname, '../data');
 
 // date format YYYYMMDD
 
@@ -20,7 +20,8 @@ let myDate = dateToYYYYMMDD(getDate);
 
 // Farsing Seoul CoronaV Page
 url = 'http://www.seoul.go.kr/coronaV/coronaStatus.do';
-resultArr = [];
+
+const resultJson = {};
 request.get(url, (error, res, html) => {
   if (error) {
     throw error;
@@ -29,7 +30,24 @@ request.get(url, (error, res, html) => {
   // get update-time (source data)
   const $sourceDate = $('h5.update-date').children('strong').eq(0);
   console.log($sourceDate.text());
+  resultJson.date = $sourceDate.text();
   const $allData = $('table.status-datatable').children('tbody');
+  const $confirmedIndex = $allData.children('tr').children('th').children('p');
+  const $confiremdInfo = $allData.children('tr').children('td');
+  // console.log($confirmedIndex[1].children[0].data);
+  for (var i = 0; i < $confirmedIndex.length; i++) {
+    // console.log($confirmedIndex[i].children[0].data);
+    resultJson[$confirmedIndex[i].children[0].data] = {
+      patienId: $confiremdInfo[i * 6].children[0].data,
+      confiremdDate: $confiremdInfo[i * 6 + 1].children[0].data,
+      residence: $confiremdInfo[i * 6 + 2].children[0].data,
+      tripHistory: $confiremdInfo[i * 6 + 3].children[0].data,
+      visitType: $confiremdInfo[i * 6 + 4].children[0].data,
+      takeAction: $confiremdInfo[i * 6 + 5].children[0].data,
+    };
+  }
+  console.log(resultJson);
+  /* old way
   // change to array
   const dataArr = $allData.children('tr').text().split('\n');
   // replace \t
@@ -38,6 +56,7 @@ request.get(url, (error, res, html) => {
     dataArr[i] = dataArr[i].replace(regExpTab, '');
     // console.log(i + ' : ' + dataArr[i]);
   }
+  */
   // data Save (YYYYMMDD.txt)
-  fs.writeFileSync(`${dataDir}/${myDate}.txt`, dataArr.join('\n'), 'utf8');
+  fs.writeFileSync(`${dataDir}/${myDate}.txt`, JSON.stringify(resultJson), 'utf8');
 });
