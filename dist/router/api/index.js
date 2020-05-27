c6a6a5141d4f27069ede68471fdc4e2cdc7ea58f
a101201031 <0825tngus@naver.com>
2020-05-28 00:02:44 +0900

    feat : adding getCoronaInfo Api

--- dist/router/api/index.js
+++ dist/router/api/index.js
@@ -7,5 +7,26 @@ exports.apiRouter = void 0;
 
 var _express = require("express");
 
+var _path = require("path");
+
+var _fs = _interopRequireDefault(require("fs"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 const apiRouter = (0, _express.Router)();
 exports.apiRouter = apiRouter;
+apiRouter.get('/getCoronaInfo', (req, res) => {
+  const dataDir = (0, _path.join)(__dirname, '../../../coronaData');
+
+  _fs.default.readdir(dataDir, 'utf8', (err, files) => {
+    if (err) {
+      res.status();
+      throw err;
+    }
+
+    console.log(files);
+    console.log(dataDir);
+
+    _fs.default.readFileSync(__dirname, dataDir, files[-1]);
+  });
+});
--- src/router/api/index.js
+++ src/router/api/index.js
@@ -1,3 +1,18 @@
 import { Router, request, response } from 'express';
+import { join } from 'path';
+import fs from 'fs';
 
 export const apiRouter = Router();
+
+apiRouter.get('/getCoronaInfo', (req, res) => {
+  const dataDir = join(__dirname, '../../../coronaData');
+  fs.readdir(dataDir, 'utf8', (err, files) => {
+    if (err) {
+      res.status(500).send('not found corona data.');
+      throw err;
+    }
+    console.log(files);
+    console.log(dataDir);
+    fs.readFileSync(dataDir + files[-1], 'utf8', (err, file) => {});
+  });
+});
