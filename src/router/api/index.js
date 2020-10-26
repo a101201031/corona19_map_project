5e4621197626007d1422786c53ef679a953dd2ab
a101201031 <0825tngus@naver.com>
2020-10-26 22:33:38 +0900

    START!

--- src/router/api/index.js
+++
@@ -1,6 +0,0 @@
-import { Router, request, response } from 'express';
-
-import { confirmedCasesRouter } from './confirmedCases';
-export const apiRouter = Router();
-
-apiRouter.use('/confirmed-cases', confirmedCasesRouter);
