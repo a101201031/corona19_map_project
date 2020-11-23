ff984b86ba6f5de31f22e6d0b1772736cbd3557a
a101201031 <0825tngus@naver.com>
2020-11-24 00:20:39 +0900

    fix: fix optional chain to "!"

--- src/model/globalCoronaInfo.ts
+++ src/model/globalCoronaInfo.ts
@@ -2,18 +2,18 @@ import { prop, getModelForClass } from '@typegoose/typegoose';
 
 class GlobalCoronaInfo {
   @prop({ required: true })
-  public newConfirmed?: Number;
+  public newConfirmed!: Number;
   @prop({ required: true })
-  public totalConfirmed?: Number;
+  public totalConfirmed!: Number;
   @prop({ required: true })
-  public newDeaths?: Number;
+  public newDeaths!: Number;
   @prop({ required: true })
-  public totalDeaths?: Number;
+  public totalDeaths!: Number;
   @prop({ required: true })
-  public newRecovered?: Number;
+  public newRecovered!: Number;
   @prop({ required: true })
-  public totalRecovered?: Number;
+  public totalRecovered!: Number;
   @prop({ required: true })
-  public lastUpdate?: Date;
+  public lastUpdate!: Date;
 }
 export const GlobalCoronaInfoModel = getModelForClass(GlobalCoronaInfo);
