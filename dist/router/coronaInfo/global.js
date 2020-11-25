64d9bda74301d762c435f412cffd937a3dc99e2e
a101201031 <0825tngus@naver.com>
2020-11-25 23:24:53 +0900

    feat: fix module, using interface

--- dist/router/coronaInfo/global.js
+++ dist/router/coronaInfo/global.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.globalRouter = void 0;
 const express_1 = require("express");
-const globalCoronaInfo_1 = require("../../model/globalCoronaInfo");
-const globalCoronaInfo_2 = require("../../dataSrc/globalCoronaInfo");
+const globalCoronaInfo_1 = require("../../dataSrc/globalCoronaInfo");
+const globalCoronaInfo_2 = require("../../model/globalCoronaInfo");
 exports.globalRouter = express_1.Router();
 exports.globalRouter.get('/', async (req, res) => {
     try {
-        const resData = await globalCoronaInfo_1.GlobalCoronaInfoModel.find();
+        const resData = await globalCoronaInfo_2.GlobalCoronaInfoModel.find();
         res.status(200).json(resData);
     }
     catch (err) {
@@ -16,8 +16,8 @@ exports.globalRouter.get('/', async (req, res) => {
 });
 exports.globalRouter.post('/refresh', async (req, res) => {
     try {
-        const srcData = await globalCoronaInfo_2.srcGlobalInfo();
-        globalCoronaInfo_1.GlobalCoronaInfoModel.create(srcData);
+        const srcData = await globalCoronaInfo_1.srcGlobalInfo();
+        globalCoronaInfo_2.GlobalCoronaInfoModel.create(srcData);
         res.status(200).json({ message: 'request success' });
     }
     catch (err) {
--- src/router/coronaInfo/global.ts
+++ src/router/coronaInfo/global.ts
@@ -1,14 +1,12 @@
-import { Router, Request, Response } from 'express';
-import axios from 'axios';
-
-import { GlobalCoronaInfoModel } from '../../model/globalCoronaInfo';
+import { Request, Response, Router } from 'express';
 import { srcGlobalInfo } from '../../dataSrc/globalCoronaInfo';
+import { GlobalCoronaInfoModel, GlobalCoronaInfoTypes } from '../../model/globalCoronaInfo';
 
 export const globalRouter = Router();
 
 globalRouter.get('/', async (req: Request, res: Response) => {
   try {
-    const resData = await GlobalCoronaInfoModel.find();
+    const resData: GlobalCoronaInfoTypes[] = await GlobalCoronaInfoModel.find();
     res.status(200).json(resData);
   } catch (err) {
     res.status(500).json({ message: 'SERVER ERROR.' });
@@ -17,7 +15,7 @@ globalRouter.get('/', async (req: Request, res: Response) => {
 
 globalRouter.post('/refresh', async (req: Request, res: Response) => {
   try {
-    const srcData = await srcGlobalInfo();
+    const srcData: GlobalCoronaInfoTypes = await srcGlobalInfo();
     GlobalCoronaInfoModel.create(srcData);
     res.status(200).json({ message: 'request success' });
   } catch (err) {
