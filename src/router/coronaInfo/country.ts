12bae5088c6cccfb2a2891465311587667b8db17
a101201031 <0825tngus@naver.com>
2020-12-03 21:45:19 +0900

    fix: fix body in req to query params #5

--- src/router/coronaInfo/country.ts
+++ src/router/coronaInfo/country.ts
@@ -9,22 +9,31 @@ interface resCountryTypes {
 }
 
 countryRouter.get('/', async (req: Request, res: Response) => {
-  const { body } = req;
   const resData: resCountryTypes = {
     message: '',
   };
   try {
-    if (body.CountryCode) {
-      const data: CountryCoronaInfoTypes | null = await CountryCoronaInfoModel.findOne({
-        CountryCode: body.CountryCode,
-      });
-      resData.data = data;
-      resData.message = resData.data ? 'success.' : 'not found country.';
-    } else {
-      const data: CountryCoronaInfoTypes[] = await CountryCoronaInfoModel.find();
-      resData.data = data;
-      resData.message = 'success.';
-    }
+    const data: CountryCoronaInfoTypes[] = await CountryCoronaInfoModel.find();
+    resData.data = data;
+    resData.message = resData.data ? 'success.' : 'not found data.';
+    res.status(resData ? 200 : 400).json(resData);
+  } catch (err) {
+    resData.message = 'SERVER ERROR.';
+    res.status(500).json(resData);
+  }
+});
+
+countryRouter.get('/:CountryCode', async (req: Request, res: Response) => {
+  const reqCountryCode = req.params.CountryCode;
+  const resData: resCountryTypes = {
+    message: '',
+  };
+  try {
+    const data: CountryCoronaInfoTypes | null = await CountryCoronaInfoModel.findOne({
+      CountryCode: reqCountryCode.toUpperCase(),
+    });
+    resData.data = data;
+    resData.message = resData.data ? 'success.' : 'not found country.';
     res.status(resData.data ? 200 : 400).json(resData);
   } catch (err) {
     resData.message = 'SERVER ERROR.';
